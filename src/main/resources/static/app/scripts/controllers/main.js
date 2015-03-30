7b0af1e173c6a1d223682c57104362b63a09576a
Cosmin Juravle <cosmin.juravle@nespresso.com>
2015-03-30 11:01:46 +0200

    further updated build.gradle deps

--- src/main/resources/static/app/scripts/controllers/main.js
+++ src/main/resources/static/app/scripts/controllers/main.js
@@ -8,12 +8,17 @@
  * Controller of the webclientApp
  */
 angular.module('webclientApp')
-  .controller('MainCtrl', function ($resource) {
+.controller('MainCtrl', function ($resource) {
     this.customer = {id: null, name: 'guest'};
     //var Customer = $resource('/api/customer/:customerId', {customerId: '@id'});
     var Customer = $resource('/server/myDetails');
     var main = this;
-    Customer.get().$promise.then(function(customer) {
-      main.customer = customer;
+    Customer.get().$promise.then(function (customer) {
+        main.customer = customer;
     });
-  });
+
+    this.showVideo = function () {
+        return this.customer.role == 'CUSTOMER';
+    }
+
+});
